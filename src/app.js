e7a3d794ffc448a21867b479b97525077ba75dce
nivesb <91502753+nivesb13@users.noreply.github.com>
2021-12-06 21:56:11 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -32,8 +32,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "fdaa0bb1bbe3059f740bdeb5180d2b46";
-let city = "Varaždin";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "fdaa0bb1bbe3059f740bdeb5180d2b46";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Varaždin");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
