97ea8a3401eb9069fed6a03a9ad7a36b46e9a70e
nivesb <91502753+nivesb13@users.noreply.github.com>
2021-12-06 20:12:56 +0100

    moved percitipation and console.log

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "fdaa0bb1bbe3059f740bdeb5180d2b46";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Varaždin&appid=${apiKey}&units=metric`;
+let city = "Varaždin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
