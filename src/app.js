7e592ccd8c027ff1d71a17c33ae983041aa5f791
nivesb <91502753+nivesb13@users.noreply.github.com>
2021-12-06 19:48:50 +0100

    real-tepm

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.main.temp;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#descrioption");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "fdaa0bb1bbe3059f740bdeb5180d2b46";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Varaždin&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Varaždin&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
